ae62c80b1a2dbc3d821c7f0c60126a64759d0ced
Kleber Lima <mov_kleber@hotmail.com>
2020-09-28 23:52:59 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_category (name) VALUES('Books');
+INSERT INTO tb_category (name) VALUES('Eloctronics');
+INSERT INTO tb_category (name) VALUES('Computes');
+INSERT INTO tb_category (name) VALUES('Animals');
