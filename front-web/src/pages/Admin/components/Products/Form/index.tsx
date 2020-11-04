73817650c71acc8393f8e9bd33203c7f4f609f6f
kleberlimagit <mov_kleber@hotmail.com>
2020-11-04 19:24:24 -0300

    Estrutura do base form admin

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -4,7 +4,13 @@ import './styles.scss';
 
 const Form = () => {
     return (
-        <BaseForm/>
+        <BaseForm title="cadastrar um produto">
+            <div className="row">
+                <div className="col-6">
+                    <input type="text" className="form-control"/>
+                </div>
+            </div>
+        </BaseForm>
     )
 }
 
