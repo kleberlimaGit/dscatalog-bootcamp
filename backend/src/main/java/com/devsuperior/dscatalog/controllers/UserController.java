245435e9284edb6e252a431295994656d171a2b7
kleberlimagit <mov_kleber@hotmail.com>
2020-11-16 20:57:06 -0300

    User Controller

---
+++ backend/src/main/java/com/devsuperior/dscatalog/controllers/UserController.java
@@ -0,0 +1,81 @@
+package com.devsuperior.dscatalog.controllers;
+
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+	
+	
+	//=============================================== PAGINAÇAO ======================================================
+	@GetMapping
+	public ResponseEntity<Page<UserDTO>> findAll(
+			
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "firstName") String orderBy){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<UserDTO> list = service.findAllPaged(pageRequest);
+		
+		return ResponseEntity.ok().body(list);
+	}
+	//================================================ BUSCAR POR ID ===================================================
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+		UserDTO dto = service.findById(id);
+		
+		return ResponseEntity.ok().body(dto);
+	}
+	//================================================ Novo Usuário ===================================================
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto){
+		UserDTO newDto = service.insert(dto);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto); // codigo que deve retornar é o 201 de recurso criado
+	}
+	
+	//================================================ Atualizar Usuário ===================================================
+	@PutMapping(value="/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable Long id ,@RequestBody UserDTO dto){
+		dto = service.update(id,dto);
+			return ResponseEntity.ok().body(dto); // codigo que deve retornar é o 201 de recurso criado
+	}
+	//================================================ Delete Usuário ===================================================
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<UserDTO> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build(); // codigo que deve retornar é o 204 
+	}
+	
+	
+	
+}
