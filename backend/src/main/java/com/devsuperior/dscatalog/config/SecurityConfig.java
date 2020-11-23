f6af63f91903946a3870d8c90cb25b1ef48676e7
kleberlimagit <mov_kleber@hotmail.com>
2020-11-22 22:11:14 -0300

    Configuraçao oauth2

--- backend/src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/SecurityConfig.java
@@ -1,16 +1,43 @@
 package com.devsuperior.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired 
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	
+	// este método analisa o email e a senha do usuário.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/**");
+		web.ignoring().antMatchers("/actuator/**");
 	}
+	
+	@Override
+	@Bean // torna o authenticationManager um componente bean do sistema 
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	
 }
