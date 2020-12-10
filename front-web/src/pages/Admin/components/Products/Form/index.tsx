bf8ec1aaa4585967181b5609e8c0b3a3bed46a10
kleberlimagit <mov_kleber@hotmail.com>
2020-12-09 23:13:28 -0300

    Trazer as categorias ao editar

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { makePrivateRequest, makeRequest } from 'core/utils/request';
-import { toast} from 'react-toastify';
-import  Select from 'react-select';
-import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import Select from 'react-select';
+import { useForm, Controller } from 'react-hook-form';
 import BaseForm from '../../BaseForm';
 import { useHistory, useParams } from 'react-router-dom';
 import './styles.scss';
 import { Category } from 'core/types/Products';
 
 type FormState = {
-    name: string,
-    price: string,
-    description: string,
+    name: string;
+    price: string;
+    description: string;
+    categories: Category[];
     imgUrl: string;
 }
 
@@ -20,65 +21,64 @@ type ParamsType = {
 }
 
 const Form = () => {
-    const { register, handleSubmit, errors, setValue } = useForm<FormState>();
+    const { register, handleSubmit, errors, setValue, control } = useForm<FormState>();
     const history = useHistory();
-    const {productId} = useParams<ParamsType>();
-    const [isLoadingCategories,setIsLoadingCategories] = useState(false)
-    const [categories,setCategories] = useState<Category[]>([])
+    const { productId } = useParams<ParamsType>();
+    const [isLoadingCategories, setIsLoadingCategories] = useState(false)
+    const [categories, setCategories] = useState<Category[]>([])
     const isEditing = productId !== 'create';
-    useEffect(()=> {
-        if(isEditing){
-            makeRequest({url: `/products/${productId}`})
-            .then(response => {
-                setValue('name',response.data.name)
-                setValue('price',response.data.price)
-                setValue('description',response.data.description)
-                setValue('imgUrl',response.data.imgUrl)
-            })
+
+    useEffect(() => {
+        if (isEditing) {
+            makeRequest({ url: `/products/${productId}` })
+                .then(response => {
+                    setValue('name', response.data.name)
+                    setValue('price', response.data.price)
+                    setValue('description', response.data.description)
+                    setValue('imgUrl', response.data.imgUrl)
+                    setValue('categories',response.data.categories)
+                })
         }
-        else{
-            console.log("cadastrar")
-        }
 
+    }, [productId, isEditing, setValue])
 
-    }, [productId,isEditing,setValue])
-
-    useEffect(()=>{
+    useEffect(() => {
         setIsLoadingCategories(true)
-        makeRequest({url:'/categories'})
-        .then(response => setCategories(response.data.content))
-        .finally(()=>{
-            setIsLoadingCategories(false)
-        })
-    },[]);
+        makeRequest({ url: '/categories' })
+            .then(response => setCategories(response.data.content))
+            .finally(() => {
+                setIsLoadingCategories(false)
+            })
+    }, []);
 
     const onSubmit = (data: FormState) => {
-        makePrivateRequest({ 
-            url: isEditing ? `/products/${productId}` : '/products', 
-            method: isEditing ? 'PUT':'POST',
-            data })
-        .then(()=>{
-            toast.success(' Produto cadastrado com sucesso!', {
-                autoClose: 2500
-            });
-            history.push('/admin/products');
+        makePrivateRequest({
+            url: isEditing ? `/products/${productId}` : '/products',
+            method: isEditing ? 'PUT' : 'POST',
+            data
         })
-        .catch(()=>{
-            toast.error('Erro ao salvar produto');
-        })
+            .then(() => {
+                toast.success(' Produto cadastrado com sucesso!', {
+                    autoClose: 2500
+                });
+                history.push('/admin/products');
+            })
+            .catch(() => {
+                toast.error('Erro ao salvar produto');
+            })
     }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <BaseForm title= {`${isEditing ? 'editar produto': 'cadastrar um produto'}`} buttonName={`${isEditing ? 'SALVAR': 'CADASTRAR'}`}>
+            <BaseForm title={`${isEditing ? 'editar produto' : 'cadastrar um produto'}`} buttonName={`${isEditing ? 'SALVAR' : 'CADASTRAR'}`}>
                 <div className="row">
                     <div className="col-6">
                         <div className="margin-input-32">
                             <input
                                 ref={register({
                                     required: "Campo obrigatório",
-                                    minLength:{value :5, message: 'O campo deve ter no mínimo 5 caracteres.'},
-                                    maxLength:{value:60, message: 'O campo deve ter no máximo 60 caracteres.'}
+                                    minLength: { value: 5, message: 'O campo deve ter no mínimo 5 caracteres.' },
+                                    maxLength: { value: 60, message: 'O campo deve ter no máximo 60 caracteres.' }
                                 })}
                                 type="text"
                                 name="name"
@@ -92,13 +92,23 @@ const Form = () => {
                         </div>
 
                         <div className="margin-input-32">
-                            <Select options={categories}
-                                getOptionLabel={(option: Category)=> option.name}
-                                getOptionValue={(option:Category)=> String(option.id)}
+                            <Controller
+                                as={Select}
+                                name="categories"
+                                rules={{ required: true }}
+                                control={control}
+                                isLoading={isLoadingCategories}
+                                options={categories}
+                                getOptionLabel={(option: Category) => option.name}
+                                getOptionValue={(option: Category) => String(option.id)}
                                 classNamePrefix="categories-select"
-                                placeholder="Categoria"
+                                placeholder="Categorias"
                                 isMulti
                             />
+                            {errors.categories && (
+                                <div className="invalid-feedback d-block">
+                                    Campo obrigatório
+                                </div>)}
                         </div>
 
                         <div className="margin-input-32">
