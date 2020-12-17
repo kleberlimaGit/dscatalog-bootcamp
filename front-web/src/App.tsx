f478fe4a17c72287ebb1d468a40ed8c2dfa482c4
kleberlimagit <mov_kleber@hotmail.com>
2020-12-17 13:04:55 -0300

    logout após expiração do tempo do token

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { isTimeExpired } from './core/utils/auth'
 import './core/assets/styles/custom.scss';
 import './app.scss';
 import Routes from './Routes';
@@ -9,8 +10,9 @@ import Routes from './Routes';
 
 
 const App = () =>{
+    isTimeExpired();
     return (
-        <>
+        <>        
             <Routes/>
             <ToastContainer/>
         </>
